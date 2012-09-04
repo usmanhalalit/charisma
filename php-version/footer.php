1878df4630f7d8136df0e43a835fba8f76541a80
Muhammad Usman <mdusyl@yahoo.com>
2012-09-04 14:29:25 +0600

    Added Google Analytics for demo site only

--- php-version/footer.php
+++ php-version/footer.php
@@ -102,6 +102,20 @@
 	<script src="js/jquery.history.js"></script>
 	<!-- application script for Charisma demo -->
 	<script src="js/charisma.js"></script>
-
+	
+	<?php //Google Analytics code for tracking my demo site, you can remove this.
+		if($_SERVER['HTTP_HOST']=='usman.it') { ?>
+		<script>
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-26532312-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+			})();
+		</script>
+	<?php } ?>
+	
 </body>
 </html>
