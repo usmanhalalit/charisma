e20e2d0ef4ced5cff7b0a25565b1a57a64be6776
smid <jan@smid.sk>
2013-03-03 00:28:35 +0100

    https://github.com/j3nda/admin-template-charisma/issues/6

--- js/login.js
+++ js/login.js
@@ -4,11 +4,11 @@ jQuery.fn.center = function () {
     this.css("left", Math.max(0, (($(window).width()  - $(this).outerWidth() ) / 2) + $(window).scrollLeft()) + "px");
 
     return this;
-}
+};
 
 
 $(window).resize(function() {
-//    $('.login-box').center();
+    $('.login-box').center();
 });
 
 
@@ -16,25 +16,27 @@ $(document).ready(function() {
     var backup = {
         'login': $('fieldset #login').html()
     };
-//    $('.login-box').center();
+    $('.login-box').center();
     $('fieldset #forgotel').css('visibility', 'hidden');
     $('fieldset input[name=forgotemail]').attr('disabled', true);
 
     $('fieldset #forgotch').change(function() {
         var el = {
-            'username': $('fieldset input[name=username]'),
-            'password': $('fieldset input[name=password]'),
-            'remember': $('fieldset input[name=remember]'),
-            'forgotin': $('fieldset input[name=forgotemail]'),
-            'forgotch': $('fieldset #forgotch'),
-            'forgotel': $('fieldset #forgotel'),
-            'login'   : $('fieldset #login')
+            'username' : $('fieldset input[name=username]'),
+            'password' : $('fieldset input[name=password]'),
+            'remember' : $('fieldset input[name=remember]'),
+			'dremember': $('fieldset .remember'),
+            'forgotin' : $('fieldset input[name=forgotemail]'),
+            'forgotch' : $('fieldset #forgotch'),
+            'forgotel':  $('fieldset #forgotel'),
+            'login'    : $('fieldset #login')
         };
 
 
         if (el['forgotch'].prop('checked')) {
             el['remember'].attr('disabled', true);
             el['remember'].attr('readonly', true);
+			el['dremember'].toggleClass('disabled');
 
             el['username'].attr('disabled', true);
             el['password'].attr('disabled', true);
@@ -48,6 +50,7 @@ $(document).ready(function() {
         } else {
             el['remember'].removeAttr("disabled");
             el['remember'].removeAttr("readonly");
+			el['dremember'].toggleClass('disabled');
 
             el['username'].removeAttr('disabled');
             el['password'].removeAttr('disabled');
