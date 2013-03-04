df5c781a337b3d4aeafe5a49b079606eeedb9dd6
j3nda <jan@smid.sk>
2013-03-04 21:37:58 +0100

    new css theme: zatebou.cz (~for my own project) based on "s2" css theme; and this new css theme into javascript-theme-switcher (~right top on the page)

--- js/charisma.js
+++ js/charisma.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	//themes, change CSS with JS
 	//default theme(CSS) is cerulean, change it if needed
-	var current_theme = $.cookie('current_theme')==null ? 's2' :$.cookie('current_theme');
+	var current_theme = $.cookie('current_theme')==null ? 'zatebou' :$.cookie('current_theme');
 	switch_theme(current_theme);
 	
 	
