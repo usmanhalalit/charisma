6cb939ddde61c7100e948c3f01f061da359e22db
j3nda <jan@smid.sk>
2013-03-06 19:51:00 +0100

    fix: some minor issues with formating js to better [code-standards]

--- js/login.js
+++ js/login.js
@@ -22,46 +22,46 @@ $(document).ready(function() {
 	$('fieldset input[name=forgotemail]').attr('disabled', true);
 
 	$('fieldset #forgotch').change(function() {
-	var el = {
-		'username' : $('fieldset input[name=username]'),
-		'password' : $('fieldset input[name=password]'),
-		'remember' : $('fieldset input[name=remember]'),
-		'dremember': $('fieldset .remember'),
-		'forgotin' : $('fieldset input[name=forgotemail]'),
-		'forgotch' : $('fieldset #forgotch'),
-		'forgotel':  $('fieldset #forgotel'),
-		'login'    : $('fieldset #login')
-	};
+		var el = {
+			'username' : $('fieldset input[name=username]'),
+			'password' : $('fieldset input[name=password]'),
+			'remember' : $('fieldset input[name=remember]'),
+			'dremember': $('fieldset .remember'),
+			'forgotin' : $('fieldset input[name=forgotemail]'),
+			'forgotch' : $('fieldset #forgotch'),
+			'forgotel':  $('fieldset #forgotel'),
+			'login'    : $('fieldset #login')
+		};
 
 
-	if (el['forgotch'].prop('checked')) {
-		el['remember'].attr('disabled', true);
-		el['remember'].attr('readonly', true);
-		el['dremember'].toggleClass('disabled');
+		if (el['forgotch'].prop('checked')) {
+			el['remember'].attr('disabled', true);
+			el['remember'].attr('readonly', true);
+			el['dremember'].toggleClass('disabled');
 
-		el['username'].attr('disabled', true);
-		el['password'].attr('disabled', true);
+			el['username'].attr('disabled', true);
+			el['password'].attr('disabled', true);
 
-		el['forgotel'].css('visibility', '');
-		el['forgotin'].removeAttr("disabled");
+			el['forgotel'].css('visibility', '');
+			el['forgotin'].removeAttr("disabled");
 
-		el['login'].html('Send');
-		el['login'].toggleClass('btn-forgot');
+			el['login'].html('Send');
+			el['login'].toggleClass('btn-forgot');
 
-	} else {
-		el['remember'].removeAttr("disabled");
-		el['remember'].removeAttr("readonly");
-		el['dremember'].toggleClass('disabled');
+		} else {
+			el['remember'].removeAttr("disabled");
+			el['remember'].removeAttr("readonly");
+			el['dremember'].toggleClass('disabled');
 
-		el['username'].removeAttr('disabled');
-		el['password'].removeAttr('disabled');
+			el['username'].removeAttr('disabled');
+			el['password'].removeAttr('disabled');
 
 
-		el['forgotel'].css('visibility', 'hidden');
-		el['forgotin'].attr('disabled', true);
+			el['forgotel'].css('visibility', 'hidden');
+			el['forgotin'].attr('disabled', true);
 
-		el['login'].html(backup['login']);
-		el['login'].toggleClass('btn-forgot');
-	}
+			el['login'].html(backup['login']);
+			el['login'].toggleClass('btn-forgot');
+		}
 	});
 });
