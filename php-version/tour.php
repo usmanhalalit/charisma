a696e0d4fb48a5f96cd1f2fabcbf647d77a98ddc
Muhammad Usman <mdusyl@yahoo.com>
2014-01-06 00:06:57 +0600

    Refactored tour.php

--- php-version/tour.php
+++ php-version/tour.php
@@ -1,7 +1,5 @@
-<?php include('header.php'); ?>
-
-
-			<div>
+<?php require('header.php'); ?>
+<div>
 				<ul class="breadcrumb">
 					<li>
 						<a href="#">Home</a> <span class="divider">/</span>
@@ -13,20 +11,20 @@
 			</div>
 
 			<div class="row sortable">
-				<div class="box col-md-12 tour">
-					<div class="box-header well" data-original-title>
+				<div class="box col-md-12 tour"><div class="box-inner">
+					<div class="box-header well" data-original-title="">
 						<h2><i class="icon-globe"></i> Tour</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="icon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="icon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="icon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="icon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
-						<a href="tour.html">Click Here to restart the tour</a><br/>
-						You can create Custom Tour, like this. <br/> For more help on implementing tour go <a href="http://pushly.github.com/bootstrap-tour/index.html" target="_blank">here</a>
+						<a href="tour.html">Click Here to restart the tour</a><br>
+						You can create Custom Tour, like this. <br> For more help on implementing tour go <a href="http://pushly.github.com/bootstrap-tour/index.html" target="_blank">here</a>
 					</div>
-				</div><!--/span-->
+				</div></div><!--/span-->
 			</div><!--/row-->
 
-<?php include('footer.php'); ?>
+<?php require('footer.php'); ?>
