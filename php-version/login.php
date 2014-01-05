ede7d2de3c610cbfab3b66da1861e1d658b016e5
Muhammad Usman <mdusyl@yahoo.com>
2014-01-06 00:14:43 +0600

    Refactored login.

--- php-version/login.php
+++ php-version/login.php
@@ -16,17 +16,17 @@ include('header.php'); ?>
 					<form class="form-horizontal" action="index.html" method="post">
 						<fieldset>
 							<div class="input-prepend" title="Username" data-rel="tooltip">
-								<span class="add-on"><i class="icon-user"></i></span><input autofocus class="input-large col-md-0" name="username" id="username" type="text" value="admin" />
+								<span class="input-group-addon"><i class="icon-user"></i></span><input autofocus="" class="col-md-0 input-lg" name="username" id="username" type="text" value="admin">
 							</div>
 							<div class="clearfix"></div>
 
 							<div class="input-prepend" title="Password" data-rel="tooltip">
-								<span class="add-on"><i class="icon-lock"></i></span><input class="input-large col-md-0" name="password" id="password" type="password" value="admin123456" />
+								<span class="input-group-addon"><i class="icon-lock"></i></span><input class="col-md-0 input-lg" name="password" id="password" type="password" value="admin123456">
 							</div>
 							<div class="clearfix"></div>
 
 							<div class="input-prepend">
-							<label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
+							<label class="remember" for="remember"><input type="checkbox" id="remember">Remember me</label>
 							</div>
 							<div class="clearfix"></div>
 
@@ -37,4 +37,4 @@ include('header.php'); ?>
 					</form>
 				</div><!--/span-->
 			</div><!--/row-->
-<?php include('footer.php'); ?>
+<?php require('footer.php'); ?>
