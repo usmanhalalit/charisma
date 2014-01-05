ab25163c16c600e147218850763cdc7f89cf9830
Muhammad Usman <mdusyl@yahoo.com>
2014-01-06 00:05:33 +0600

    Refactored file-manager.

--- php-version/file-manager.php
+++ php-version/file-manager.php
@@ -1,6 +1,5 @@
-<?php include('header.php'); ?>
-
-			<div>
+<?php require('header.php'); ?>
+<div>
 				<ul class="breadcrumb">
 					<li>
 						<a href="#">Home</a> <span class="divider">/</span>
@@ -12,25 +11,25 @@
 			</div>
 
 			<div class="row sortable">
-				<div class="box col-md-12">
-					<div class="box-header well" data-original-title>
+				<div class="box col-md-12"><div class="box-inner">
+					<div class="box-header well" data-original-title="">
 						<h2><i class="glyphicon glyphicon-picture"></i> File Manager</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="glyphicon glyphicon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="glyphicon glyphicon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="glyphicon glyphicon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
 						<div class="alert alert-info">
-							<button type="button" class="close" data-dismiss="alert">×</button>
+							<button type="button" class="close" data-dismiss="alert">&times;</button>
 							<i class="glyphicon glyphicon-info-sign"></i> As its a demo, you currently have read-only permission, in your server you may do everything like, upload or delete. It will work on a server only.
 						</div>
 						<div class="file-manager"></div>
 					</div>
-				</div><!--/span-->
+				</div></div><!--/span-->
 			
 			</div><!--/row-->
 
-		
-<?php include('footer.php'); ?>
+
+<?php require('footer.php'); ?>
