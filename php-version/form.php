12ee143e1fbdb0975f264143620042fa9605db4f
Muhammad Usman <mdusyl@yahoo.com>
2014-01-05 23:47:18 +0600

    Refa.. form.php.

--- php-version/form.php
+++ php-version/form.php
@@ -1,7 +1,5 @@
-<?php include('header.php'); ?>
-
-
-			<div>
+<?php require('header.php'); ?>
+<div>
 				<ul class="breadcrumb">
 					<li>
 						<a href="#">Home</a> <span class="divider">/</span>
@@ -13,40 +11,40 @@
 			</div>
 			
 			<div class="row sortable">
-				<div class="box col-md-12">
-					<div class="box-header well" data-original-title>
+				<div class="box col-md-12"><div class="box-inner">
+					<div class="box-header well">
 						<h2><i class="glyphicon glyphicon-edit"></i> Form Elements</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="glyphicon glyphicon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="glyphicon glyphicon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="glyphicon glyphicon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
 						<form class="form-horizontal">
 						  <fieldset>
 							<legend>Datepicker, Autocomplete, WYSIWYG</legend>
-							<div class="control-group">
+							<div class="form-group">
 							  <label class="control-label" for="typeahead">Auto complete </label>
 							  <div class="controls">
-								<input type="text" class="col-md-6 typeahead" id="typeahead"  data-provide="typeahead" data-items="4" data-source='["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]'>
+								<input type="text" class="col-md-6 typeahead" id="typeahead" data-provide="typeahead" data-items="4" data-source="[&quot;Alabama&quot;,&quot;Alaska&quot;,&quot;Arizona&quot;,&quot;Arkansas&quot;,&quot;California&quot;,&quot;Colorado&quot;,&quot;Connecticut&quot;,&quot;Delaware&quot;,&quot;Florida&quot;,&quot;Georgia&quot;,&quot;Hawaii&quot;,&quot;Idaho&quot;,&quot;Illinois&quot;,&quot;Indiana&quot;,&quot;Iowa&quot;,&quot;Kansas&quot;,&quot;Kentucky&quot;,&quot;Louisiana&quot;,&quot;Maine&quot;,&quot;Maryland&quot;,&quot;Massachusetts&quot;,&quot;Michigan&quot;,&quot;Minnesota&quot;,&quot;Mississippi&quot;,&quot;Missouri&quot;,&quot;Montana&quot;,&quot;Nebraska&quot;,&quot;Nevada&quot;,&quot;New Hampshire&quot;,&quot;New Jersey&quot;,&quot;New Mexico&quot;,&quot;New York&quot;,&quot;North Dakota&quot;,&quot;North Carolina&quot;,&quot;Ohio&quot;,&quot;Oklahoma&quot;,&quot;Oregon&quot;,&quot;Pennsylvania&quot;,&quot;Rhode Island&quot;,&quot;South Carolina&quot;,&quot;South Dakota&quot;,&quot;Tennessee&quot;,&quot;Texas&quot;,&quot;Utah&quot;,&quot;Vermont&quot;,&quot;Virginia&quot;,&quot;Washington&quot;,&quot;West Virginia&quot;,&quot;Wisconsin&quot;,&quot;Wyoming&quot;]">
 								<p class="help-block">Start typing to activate auto complete!</p>
 							  </div>
 							</div>
-							<div class="control-group">
+							<div class="form-group">
 							  <label class="control-label" for="date01">Date input</label>
 							  <div class="controls">
 								<input type="text" class="input-xlarge datepicker" id="date01" value="02/16/12">
 							  </div>
 							</div>
 
-							<div class="control-group">
+							<div class="form-group">
 							  <label class="control-label" for="fileInput">File input</label>
 							  <div class="controls">
 								<input class="input-file uniform_on" id="fileInput" type="file">
 							  </div>
 							</div>          
-							<div class="control-group">
+							<div class="form-group">
 							  <label class="control-label" for="textarea2">Textarea WYSIWYG</label>
 							  <div class="controls">
 								<textarea class="cleditor" id="textarea2" rows="3"></textarea>
@@ -54,49 +52,49 @@
 							</div>
 							<div class="form-actions">
 							  <button type="submit" class="btn btn-primary">Save changes</button>
-							  <button type="reset" class="btn">Cancel</button>
+							  <button type="reset" class="btn btn-default">Cancel</button>
 							</div>
 						  </fieldset>
 						</form>   
 
 					</div>
-				</div><!--/span-->
+				</div></div><!--/span-->
 
 			</div><!--/row-->
 
 
 			<div class="row sortable">
-				<div class="box col-md-12">
-					<div class="box-header well" data-original-title>
+				<div class="box col-md-12"><div class="box-inner">
+					<div class="box-header well" data-original-title="">
 						<h2><i class="glyphicon glyphicon-edit"></i> Form Elements</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="glyphicon glyphicon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="glyphicon glyphicon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="glyphicon glyphicon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="focusedInput">Focused input</label>
 								<div class="controls">
-								  <input class="input-xlarge focused" id="focusedInput" type="text" value="This is focused…">
+								  <input class="input-xlarge focused" id="focusedInput" type="text" value="This is focusedâ€¦">
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label">Uneditable input</label>
 								<div class="controls">
 								  <span class="input-xlarge uneditable-input">Some value here</span>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="disabledInput">Disabled input</label>
 								<div class="controls">
-								  <input class="input-xlarge disabled" id="disabledInput" type="text" placeholder="Disabled input here…" disabled="">
+								  <input class="input-xlarge disabled" id="disabledInput" type="text" placeholder="Disabled input hereâ€¦" disabled="">
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="optionsCheckbox2">Disabled checkbox</label>
 								<div class="controls">
 								  <label class="checkbox">
@@ -105,28 +103,28 @@
 								  </label>
 								</div>
 							  </div>
-							  <div class="control-group warning">
+							  <div class="warning form-group">
 								<label class="control-label" for="inputWarning">Input with warning</label>
 								<div class="controls">
 								  <input type="text" id="inputWarning">
 								  <span class="help-inline">Something may have gone wrong</span>
 								</div>
 							  </div>
-							  <div class="control-group error">
+							  <div class="error form-group">
 								<label class="control-label" for="inputError">Input with error</label>
 								<div class="controls">
 								  <input type="text" id="inputError">
 								  <span class="help-inline">Please correct the error</span>
 								</div>
 							  </div>
-							  <div class="control-group success">
+							  <div class="success form-group">
 								<label class="control-label" for="inputSuccess">Input with success</label>
 								<div class="controls">
 								  <input type="text" id="inputSuccess">
 								  <span class="help-inline">Woohoo!</span>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="selectError3">Plain Select</label>
 								<div class="controls">
 								  <select id="selectError3">
@@ -138,7 +136,7 @@
 								  </select>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="selectError">Modern Select</label>
 								<div class="controls">
 								  <select id="selectError" data-rel="chosen">
@@ -150,19 +148,19 @@
 								  </select>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="selectError1">Multiple Select / Tags</label>
 								<div class="controls">
-								  <select id="selectError1" multiple data-rel="chosen">
+								  <select id="selectError1" multiple="" data-rel="chosen">
 									<option>Option 1</option>
-									<option selected>Option 2</option>
+									<option selected="">Option 2</option>
 									<option>Option 3</option>
 									<option>Option 4</option>
 									<option>Option 5</option>
 								  </select>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="selectError2">Group Select</label>
 								<div class="controls">
 									<select data-placeholder="Your Favorite Football Team" id="selectError2" data-rel="chosen">
@@ -220,72 +218,72 @@
 							  </div>
 							  <div class="form-actions">
 								<button type="submit" class="btn btn-primary">Save changes</button>
-								<button class="btn">Cancel</button>
+								<button class="btn btn-default">Cancel</button>
 							  </div>
 							</fieldset>
 						  </form>
 					
 					</div>
-				</div><!--/span-->
+				</div></div><!--/span-->
 			
 			</div><!--/row-->
 			
 			<div class="row sortable">
-				<div class="box col-md-12">
-					<div class="box-header well" data-original-title>
+				<div class="box col-md-12"><div class="box-inner">
+					<div class="box-header well" data-original-title="">
 						<h2><i class="glyphicon glyphicon-edit"></i> Form Elements</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="glyphicon glyphicon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="glyphicon glyphicon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="glyphicon glyphicon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="prependedInput">Prepended text</label>
 								<div class="controls">
 								  <div class="input-prepend">
-									<span class="add-on">@</span><input id="prependedInput" size="16" type="text">
+									<span class="input-group-addon">@</span><input id="prependedInput" size="16" type="text">
 								  </div>
 								  <p class="help-block">Here's some help text</p>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="appendedInput">Appended text</label>
 								<div class="controls">
 								  <div class="input-append">
-									<input id="appendedInput" size="16" type="text"><span class="add-on">.00</span>
+									<input id="appendedInput" size="16" type="text"><span class="input-group-addon">.00</span>
 								  </div>
 								  <span class="help-inline">Here's more help text</span>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="appendedPrependedInput">Append and prepend</label>
 								<div class="controls">
 								  <div class="input-prepend input-append">
-									<span class="add-on">$</span><input id="appendedPrependedInput" size="16" type="text"><span class="add-on">.00</span>
+									<span class="input-group-addon">$</span><input id="appendedPrependedInput" size="16" type="text"><span class="input-group-addon">.00</span>
 								  </div>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="appendedInputButton">Append with button</label>
 								<div class="controls">
 								  <div class="input-append">
-									<input id="appendedInputButton" size="16" type="text"><button class="btn" type="button">Go!</button>
+									<input id="appendedInputButton" size="16" type="text"><button class="btn btn-default" type="button">Go!</button>
 								  </div>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label" for="appendedInputButtons">Two-button append</label>
 								<div class="controls">
 								  <div class="input-append">
-									<input id="appendedInputButtons" size="16" type="text"><button class="btn" type="button">Search</button><button class="btn" type="button">Options</button>
+									<input id="appendedInputButtons" size="16" type="text"><button class="btn btn-default" type="button">Search</button><button class="btn btn-default" type="button">Options</button>
 								  </div>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label">Checkboxes</label>
 								<div class="controls">
 								  <label class="checkbox inline">
@@ -299,18 +297,18 @@
 								  </label>
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label">File Upload</label>
 								<div class="controls">
 								  <input type="file">
 								</div>
 							  </div>
-							  <div class="control-group">
+							  <div class="form-group">
 								<label class="control-label">Radio buttons</label>
 								<div class="controls">
 								  <label class="radio">
 									<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-									Option one is this and that—be sure to include why it's great
+									Option one is this and thatâ€”be sure to include why it's great
 								  </label>
 								  <div style="clear:both"></div>
 								  <label class="radio">
@@ -321,13 +319,14 @@
 							  </div>
 							  <div class="form-actions">
 								<button type="submit" class="btn btn-primary">Save changes</button>
-								<button class="btn">Cancel</button>
+								<button class="btn btn-default">Cancel</button>
 							  </div>
 							</fieldset>
 						</form>
 					</div>
-				</div><!--/span-->
+				</div></div><!--/span-->
 
 			</div><!--/row-->
-    
-<?php include('footer.php'); ?>
+
+<?php require('footer.php'); ?>
+
