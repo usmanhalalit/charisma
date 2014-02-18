5eab89962678b83c040c7311f46ec2b352809c83
Muhammad Usman <mdusyl@yahoo.com>
2014-02-18 19:13:17 +0600

    Fixed icons on typography.

--- php-version/typography.php
+++ php-version/typography.php
@@ -13,11 +13,11 @@
 			<div class="row sortable">
 				<div class="box col-md-9"><div class="box-inner">
 					<div class="box-header well" data-original-title="">
-						<h2><i class="icon-font"></i> Typography</h2>
+						<h2><i class="glyphicon glyphicon-font"></i> Typography</h2>
 						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round btn-default"><i class="icon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="icon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round btn-default"><i class="icon-remove"></i></a>
+							<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
@@ -103,7 +103,7 @@
 					<div class="box-header well" data-original-title="">
 						<h3>Unordered List</h3>
 						<div class="box-icon">
-							<a href="#" class="btn btn-close btn-round btn-default"><i class="icon-remove"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
@@ -131,7 +131,7 @@
 					<div class="box-header well" data-original-title="">
 						<h3>Ordered List</h3>
 						<div class="box-icon">
-							<a href="#" class="btn btn-close btn-round btn-default"><i class="icon-remove"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
@@ -152,7 +152,7 @@
 					<div class="box-header well" data-original-title="">
 						<h3>Description List</h3>
 						<div class="box-icon">
-							<a href="#" class="btn btn-close btn-round btn-default"><i class="icon-remove"></i></a>
+							<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
 						</div>
 					</div>
 					<div class="box-content">
