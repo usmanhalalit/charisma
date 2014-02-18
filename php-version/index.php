fc1af61861c2425816325b88e957f98be718b798
Muhammad Usman <mdusyl@yahoo.com>
2014-02-18 19:28:57 +0600

    Fixed top blocks.

--- php-version/index.php
+++ php-version/index.php
@@ -10,33 +10,41 @@
 				</ul>
 			</div>
 			<div class="sortable row">
-				<a data-rel="tooltip" title="6 new members." class="well col-md-3 top-block" href="#">
-					<span class="icon32 icon-red icon-user"></span>
-					<div>Total Members</div>
-					<div>507</div>
-					<span class="notification">6</span>
-				</a>
+                <div class="col-md-3">
+                    <a data-rel="tooltip" title="6 new members." class="well top-block" href="#">
+                        <span class="icon32 icon-red icon-user"></span>
+                        <div>Total Members</div>
+                        <div>507</div>
+                        <span class="notification">6</span>
+                    </a>
+                </div>
 
-				<a data-rel="tooltip" title="4 new pro members." class="well col-md-3 top-block" href="#">
-					<span class="icon32 icon-color icon-star-on"></span>
-					<div>Pro Members</div>
-					<div>228</div>
-					<span class="notification green">4</span>
-				</a>
+                <div class="col-md-3">
+                    <a data-rel="tooltip" title="4 new pro members." class="well top-block" href="#">
+                        <span class="icon32 icon-color icon-star-on"></span>
+                        <div>Pro Members</div>
+                        <div>228</div>
+                        <span class="notification green">4</span>
+                    </a>
+                </div>
 
-				<a data-rel="tooltip" title="$34 new sales." class="well col-md-3 top-block" href="#">
-					<span class="icon32 icon-color icon-cart"></span>
-					<div>Sales</div>
-					<div>$13320</div>
-					<span class="notification yellow">$34</span>
-				</a>
-				
-				<a data-rel="tooltip" title="12 new messages." class="well col-md-3 top-block" href="#">
-					<span class="icon32 icon-color icon-envelope-closed"></span>
-					<div>Messages</div>
-					<div>25</div>
-					<span class="notification red">12</span>
-				</a>
+                <div class="col-md-3">
+                    <a data-rel="tooltip" title="$34 new sales." class="well top-block" href="#">
+                        <span class="icon32 icon-color icon-cart"></span>
+                        <div>Sales</div>
+                        <div>$13320</div>
+                        <span class="notification yellow">$34</span>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a data-rel="tooltip" title="12 new messages." class="well top-block" href="#">
+                        <span class="icon32 icon-color icon-envelope-closed"></span>
+                        <div>Messages</div>
+                        <div>25</div>
+                        <span class="notification red">12</span>
+                    </a>
+                </div>
 			</div>
 			
 			<div class="row">
