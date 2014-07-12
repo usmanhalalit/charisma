83cab7130fa65d811cde9f44e96abd78f8f86ba5
Muhammad Usman <mdusyl@yahoo.com>
2014-07-13 01:58:57 +0600

    Fix top menu for md device.

--- php-version/table.php
+++ php-version/table.php
@@ -748,7 +748,6 @@
     <div class="box-header well" data-original-title="">
         <h2><i class="glyphicon glyphicon-user"></i> Responsive, Swipable Table</h2>
         <div class="box-icon">
-            <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
             <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
             <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
         </div>
