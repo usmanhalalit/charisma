8fa728a3d9090f82d6d52a3006c386a3b5d52672
Muhammad Usman <mdusyl@yahoo.com>
2014-07-12 14:39:07 +0600

    Add Gulp task to compile PHP files.

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var php2html = require('gulp-php2html');
+
+gulp.task('compile-php', function() {
+    gulp.src(['./php-version/*.php', '!./php-version/header.php', '!./php-version/footer.php'])
+        .pipe(php2html())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch-php', function() {
+    gulp.watch('./php-version/*.php', ['compile-php']);
+});
