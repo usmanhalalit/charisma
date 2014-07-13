80226f2865ca1544c714d6e97aaa552f620a51c6
Muhammad Usman <mdusyl@yahoo.com>
2014-07-14 00:39:17 +0600

    Fix blank page.

--- php-version/blank.php
+++ php-version/blank.php
@@ -12,22 +12,21 @@
 				</ul>
 			</div>
 
-			<div class="row">
-				<div class="box col-md-12">
-					<div class="box-header well" data-original-title>
-						<h2><i class="glyphicon glyphicon-picture"></i>Blank</h2>
-						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="glyphicon glyphicon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="glyphicon glyphicon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="glyphicon glyphicon-remove"></i></a>
-						</div>
-					</div>
-					<div class="box-content">
-						
-					</div>
-				</div><!--/span-->
-			
-			</div><!--/row-->
+            <div class="row">
+                <div class="box col-md-12"><div class="box-inner">
+                        <div class="box-header well" data-original-title="">
+                            <h2><i class="glyphicon glyphicon-star-empty"></i> Blank</h2>
+                            <div class="box-icon">
+                                <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+                                <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                                <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
+                            </div>
+                        </div>
+                        <div class="box-content">
+                            <!-- put your content here -->
+                        </div>
+                    </div></div>
+            </div><!--/row-->
 
     
 <?php include('footer.php'); ?>
--- php-version/calendar.php
+++ php-version/calendar.php
@@ -13,7 +13,7 @@
 			<div class="row">
 				<div class="box col-md-12"><div class="box-inner">
 				  <div class="box-header well" data-original-title="">
-					  <h2><i class="glyphicon glyphicon-calendar"></i>Calendar</h2>
+					  <h2><i class="glyphicon glyphicon-calendar"></i> Calendar</h2>
 					  <div class="box-icon">
 						  <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
 						  <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
