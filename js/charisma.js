5baca557186cdc8dda5218e4fd72ae2144f77350
Muhammad Usman <mdusyl@yahoo.com>
2014-07-13 18:37:21 +0600

    Fix minimize icon.

--- js/charisma.js
+++ js/charisma.js
@@ -284,8 +284,8 @@ function docReady(){
 	$('.btn-minimize').click(function(e){
 		e.preventDefault();
 		var $target = $(this).parent().parent().next('.box-content');
-		if($target.is(':visible')) $('i',$(this)).removeClass('icon-chevron-up').addClass('icon-chevron-down');
-		else 					   $('i',$(this)).removeClass('icon-chevron-down').addClass('icon-chevron-up');
+		if($target.is(':visible')) $('i',$(this)).removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		else 					   $('i',$(this)).removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 		$target.slideToggle();
 	});
 	$('.btn-setting').click(function(e){
