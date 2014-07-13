8facc9c7142628d7d1eb430d22d0f45916e98c63
Muhammad Usman <mdusyl@yahoo.com>
2014-07-13 22:56:31 +0600

    Remove easing from gallery tongue.

--- js/charisma.js
+++ js/charisma.js
@@ -172,10 +172,10 @@ function docReady() {
         $(this).append('<div class="well gallery-controls">' +
             '<p><a href="#" class="gallery-edit btn"><i class="glyphicon glyphicon-edit"></i></a> <a href="#" class="gallery-delete btn"><i class="glyphicon glyphicon-remove"></i></a></p>' +
             '</div>');
-        $(this).find('.gallery-controls').stop().animate({'margin-top': '-1'}, 400, 'easeInQuint');
+        $(this).find('.gallery-controls').stop().animate({'margin-top': '-1'}, 400);
     }, function () {
         $('img', this).fadeToggle(1000);
-        $(this).find('.gallery-controls').stop().animate({'margin-top': '-30'}, 200, 'easeInQuint', function () {
+        $(this).find('.gallery-controls').stop().animate({'margin-top': '-30'}, 200, function () {
             $(this).remove();
         });
     });
