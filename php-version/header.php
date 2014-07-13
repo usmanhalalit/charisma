8e1d1a457e035decb0a308dca3a461fb56177a0a
Muhammad Usman <mdusyl@yahoo.com>
2014-07-13 16:39:22 +0600

    Fixes.

--- php-version/header.php
+++ php-version/header.php
@@ -4,7 +4,7 @@
 <head>
 	<!--
 	    ===
-	    This comment CANNOT be removed.
+	    This comment should NOT be removed.
 
 		Charisma v2.0.0
 
@@ -18,20 +18,12 @@
 	-->
 	<meta charset="utf-8">
 	<title>Free HTML5 Bootstrap Admin Template</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
 	<meta name="author" content="Muhammad Usman">
 
 	<!-- The styles -->
 	<link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
-	<style type="text/css">
-	  body {
-		padding-bottom: 40px;
-	  }
-	  .sidebar-nav {
-		padding: 9px 0;
-	  }
-	</style>
 
 	<link href="css/charisma-app.css" rel="stylesheet">
 	<link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
